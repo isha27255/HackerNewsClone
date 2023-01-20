6f6714e4ad59ec99a02a5c3b4298da293d12b1e9
isha27255 <isha27255@gmail.com>
2023-01-20 11:04:14 +0530

    added api endpoint for displaying stories from newest to oldest

--- src/components/NewStoriesPage.js
+++ src/components/NewStoriesPage.js
@@ -19,7 +19,7 @@ class NewStoriesPage extends React.Component {
 
   componentDidMount() {
     axios
-      .get('/api/v1/search_by_date?tags=story')
+      .get('https://hn.algolia.com/api/v1/search_by_date?tags=story')
       .then(res => {
         this.setState({ stories: res.data.hits });
       })
@@ -36,14 +36,14 @@ class NewStoriesPage extends React.Component {
     const endIndex = startIndex + itemsPerPage;
     const currentItems = stories.slice(startIndex, endIndex);
     return (
-      <div class="min-h-screen bg-gray-100">
+      <div className="min-h-screen bg-gray-100">
         <DrawerAppBar></DrawerAppBar>
         
-        <div class="container mx-auto p-20">
+        <div className="container mx-auto p-20">
     {currentItems.map(story => (
-        <div key={story.objectID} class="bg-white p-6 rounded-lg shadow-lg border-t-2 border-solid border-indigo-600 p-8 mt-6">
-        <a class="text-2xl font-bold mb-2 text-gray-800" href={story.url}>{story.title} ( {story.url!=null? story.url.split("/")[2] : 'none' } )</a>
-        <p class="text-gray-500">{story.points} {story.points>1 ? 'points' : 'point'} by {story.author}  | hide | past | discuss </p>
+        <div key={story.objectID} className="bg-white p-6 rounded-lg shadow-lg border-t-2 border-solid border-indigo-600 p-8 mt-6">
+        <a className="text-2xl font-bold mb-2 text-gray-800" href={story.url}>{story.title} ( {story.url!=null? story.url.split("/")[2] : 'none' } )</a>
+        <p className="text-gray-500">{story.points} {story.points>1 ? 'points' : 'point'} by {story.author}  | hide | past | discuss </p>
 
       </div>
           ))}
@@ -64,7 +64,7 @@ class NewStoriesPage extends React.Component {
               </li>
               <li className="-ml-px">
                 {Array.from({ length: totalPages }, (_, i) => i + 1).map(pageNumber => (
-                  <button key={pageNumber} className={`relative inline-flex items-center px-4 py-2 mr-3 border border-gray-300 bg-white text-sm leading-5 font-medium rounded-full text-gray-700 ${currentPage === pageNumber ? 'bg-indigo-500 text-white' : 'hover:bg-indigo-200 hover:text-indigo-700 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500'}`} onClick={() => this.handlePageChange(pageNumber)}>
+                  <button key={pageNumber} classNameName={`relative inline-flex items-center px-4 py-2 mr-3 border border-gray-300 bg-white text-sm leading-5 font-medium rounded-full text-gray-700 ${currentPage === pageNumber ? 'bg-indigo-500 text-white' : 'hover:bg-indigo-200 hover:text-indigo-700 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500'}`} onClick={() => this.handlePageChange(pageNumber)}>
                       {pageNumber}
                     </button>
                   ))}
